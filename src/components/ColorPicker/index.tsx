c78853fde5af5e7f54fb7e8e09d6fd840dc62c0e
susiwen8 <susiwen8@gmail.com>
2021-10-29 09:22:53 +0800

    Feat: replace ColorPicker

---
+++ src/components/ColorPicker/index.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Button, Popover } from 'antd';
+import { SketchPicker } from 'react-color';
+interface Props {
+  value: string;
+  disable?: boolean;
+  onChange(color: string): void;
+}
+
+const presetColors = [
+  '#5B8FF9',
+  '#5AD8A6',
+  '#5D7092',
+  '#F6BD16',
+  '#E8684A',
+  '#6DC8EC',
+  '#9270CA',
+  '#FF9D4D',
+  '#269A99',
+  '#FF99C3',
+  '#A9ABB1',
+];
+
+export const ColorPicker = React.memo((props: Props) => {
+  const { onChange, value, disable = false } = props;
+  const onChangeComplete = React.useCallback(
+    (color) => {
+      onChange(color.hex);
+    },
+    [onChange, value],
+  );
+
+  return (
+    <Button type="text" disabled={disable} style={{ padding: '0' }}>
+      <Popover
+        trigger="click"
+        placement="left"
+        overlayClassName="color-picker-container"
+        content={
+          <SketchPicker
+            color={value}
+            disableAlpha
+            onChange={onChangeComplete}
+            presetColors={presetColors}
+          />
+        }
+      >
+        <div className="sketch-color">
+          <div className="pasta-color-icon" style={{ background: value }}></div>
+        </div>
+      </Popover>
+    </Button>
+  );
+});
