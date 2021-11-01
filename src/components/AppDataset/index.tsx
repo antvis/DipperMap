ac25b58e5c2da63cccdb573805a96adc764dba73
susiwen8 <susiwen8@gmail.com>
2021-11-01 21:12:17 +0800

    chore: add loading and replace `Paragraph` with `Tooltip`

--- src/components/AppDataset/index.tsx
+++ src/components/AppDataset/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import styles from './index.less';
-import { Tooltip, Typography } from 'antd';
-import { Button } from 'antd';
+import { Spin, Tooltip, Typography, Button } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import AddDatasetModal from './AddDatasetModal';
 import DatasetList from './DatasetList';
@@ -10,6 +9,7 @@ const { Title } = Typography;
 
 const AppDataset = () => {
   const [addDatasetVisible, setAddDatasetVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className={styles.appDataset}>
@@ -17,16 +17,25 @@ const AppDataset = () => {
         <Title level={5}>数据源</Title>
         <div>
           <Tooltip overlay={'添加数据源'}>
-            <Button
-              type="text"
-              icon={<PlusOutlined />}
-              onClick={() => setAddDatasetVisible(true)}
-            />
+            {loading ? (
+              <Spin spinning />
+            ) : (
+              <Button
+                type="text"
+                icon={<PlusOutlined />}
+                onClick={() => setAddDatasetVisible(true)}
+              />
+            )}
           </Tooltip>
         </div>
       </div>
       <DatasetList className={styles.appDatasetContent} />
-      <AddDatasetModal visible={addDatasetVisible} setVisible={setAddDatasetVisible} />
+      <AddDatasetModal
+        visible={addDatasetVisible}
+        setVisible={setAddDatasetVisible}
+        loading={loading}
+        setLoading={setLoading}
+      />
     </div>
   );
 };
