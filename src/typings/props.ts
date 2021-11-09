05ef6b869469434b15e1ca56e44b2519121d3255
yanxiong <oujinhui.ojh@antgroup.com>
2021-11-09 20:08:51 +0800

    fix: 1.修改.d.ts => .ts

---
+++ src/typings/props.ts
@@ -0,0 +1,21 @@
+export type IBaseComponentConfig<P = string> =
+  | boolean
+  | {
+      display: boolean;
+      defaultValue: P;
+    };
+
+export interface IGlobalComponentConfig {
+  mapTheme: IBaseComponentConfig<string>;
+  mapType: IBaseComponentConfig<string>;
+  preview: boolean;
+}
+
+export interface IGlobalConfig {
+  component: IGlobalComponentConfig;
+  store: boolean;
+  onChange: () => void;
+  onDatasetChange: () => void;
+  onLayerChange: () => void;
+  onFilterChange: () => void;
+}
