86dbc02c26cfb57ea18346105d629bc79544e444
yanxiong <oujinhui.ojh@antgroup.com>
2021-12-07 20:41:22 +0800

    feat: 1.修复Drawer样式问题

--- src/components/AppControl/common/AppControlItem.tsx
+++ src/components/AppControl/common/AppControlItem.tsx
@@ -17,7 +17,7 @@ const AppControlItem: React.FC<IProps> = ({
   dropdown,
   title,
   icon,
-  trigger = ['hover', 'click'],
+  trigger = ['click'],
   onActiveChange,
 }) => {
   const [active, setActive] = useState(false);
